cf7306007f01e6b3254ffcc81763143b28a5043d
Daniel Dominguez <dannickfox@gmail.com>
2016-10-28 00:55:21 -0500

    W matrix saved in 1-dimensional array

--- dft.c
+++ dft.c
@@ -1,23 +1,22 @@
 // dft.c
 // Applies the direct fourier transform algorithm.
 
-complex **gen_W (int N) {
+complex *gen_dW (int N) {
     // Generate W matrix.
     int n, k;
     double alpha;
-    complex **W = malloc(N * sizeof(complex *));
+    complex *W = malloc(N * N * sizeof(complex));
 
     for (n = 0; n < N; n++) {
-        W[n] = malloc(N * sizeof(complex));
         for (k = 0; k < N; k++) {
             alpha = -2 * M_PI * n * k / N;
-            W[n][k] = (complex) {re: cos(alpha), im: sin(alpha)};
+            W[N * n + k] = (complex) {re: cos(alpha), im: sin(alpha)};
         }
     }
     return W;
 }
 
-complex *directFourier (int N, double *x, complex **W) {
+complex *directFourier (int N, double *x, complex *W) {
     // Apply direct discrete Fourier Transform.
     complex *X = malloc(sizeof(complex) * N);
     int n, k;
@@ -25,7 +24,7 @@ complex *directFourier (int N, double *x, complex **W) {
     for (n = 0; n < N; n++) {
         X[n] = (complex) {re: 0, im: 0};
         for (k = 0; k < N; k++) {
-            X[n] = c_sum(X[n], c_scale(x[k], W[n][k]));
+            X[n] = c_sum(X[n], c_scale(x[k], W[N * n + k]));
         }
     }
     return X;
