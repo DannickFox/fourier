7a011eb41a9a39def83cffcf27a1f4d93900c725
Daniel Dominguez <dannickfox@gmail.com>
2016-10-28 00:56:06 -0500

    fW generator

--- fft.c
+++ fft.c
@@ -2,27 +2,19 @@
 // Fast fourier transform. (Radix-2 DIT)
 
 complex *gen_fW (int N) {
+    // Generates complex W array of size N / 2.
     complex *W = malloc(sizeof(complex) * N);
     double alpha;
-    for (int n = 0; n < N; n++) {
+    for (int n = 0; n < N / 2; n++) {
         if (n < N / 2) {
             alpha = -2 * M_PI * n / N;
             W[n] = (complex) {re: cos(alpha), im: sin(alpha)};
-        } else {
-            W[n] = c_neg(W[n - N / 2]);
         }
     }
     return W;
 }
 
-complex fft_sum (int n, int N, double *x, complex *W) {
-
-}
-
 complex *fastFourier(int N, double *x, complex *W) {
     complex *X = malloc(sizeof(complex) * N);
-    
-    for (int n = 0; n < N; n++) {
-    }
     return X;
 }
