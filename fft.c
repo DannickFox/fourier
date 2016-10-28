6baf8af818f6ae3da5117f3c11e3bf72b4495441
Daniel Dominguez <dannickfox@gmail.com>
2016-10-28 01:09:00 -0500

    fW size

--- fft.c
+++ fft.c
@@ -2,13 +2,14 @@
 // Fast fourier transform. (Radix-2 DIT)
 
 complex *gen_fW (int N) {
-    // Generates complex W array of size N / 2.
     complex *W = malloc(sizeof(complex) * N);
     double alpha;
-    for (int n = 0; n < N / 2; n++) {
+    for (int n = 0; n < N; n++) {
         if (n < N / 2) {
             alpha = -2 * M_PI * n / N;
             W[n] = (complex) {re: cos(alpha), im: sin(alpha)};
+        } else {
+            W[n] = c_neg(W[n - N / 2]);
         }
     }
     return W;
