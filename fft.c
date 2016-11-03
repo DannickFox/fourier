3c1530ce99840fed93b87f3e04ad7a21e7eb640c
Daniel Dominguez <dannickfox@gmail.com>
2016-11-03 09:30:29 -0600

    bitreversal

--- fft.c
+++ fft.c
@@ -16,8 +16,30 @@ complex *gen_fW (int N) {
     return W;
 }
 
+int bitCount(int N) {
+    int Nbits = 0;
+    while ((N - 1) >> ++Nbits > 0);
+    return Nbits;
+}
+
+int bitReverse (int N, int val) {
+    // Bit reversal. N must be exponent of 2.
+    int Nbits = bitCount(N);
+    int reversed = 0, x;
+
+    for (int i = 0; i < Nbits; i++) {
+        x = (val << i) & (N >> 1);
+        reversed |= x >> (Nbits - i - 1);
+    }
+    return reversed;
+}
+
 complex *fastFourier(int N, double *x, complex *W) {
     // Apply Fast Fourier Transform.
     complex *X = malloc(sizeof(complex) * N);
+    int f_idx;
+    for (int n = 0; n < N / 2; n++) {
+        
+    }
     return X;
 }
